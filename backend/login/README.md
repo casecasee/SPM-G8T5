b0520d3e49d3222d66dee7a3d03c8cf293a80ece
casecasee <cassey.low.si.yun@gmail.com>
2025-09-17 22:52:34 +0800

    Login complete (w documentation)

---
+++ backend/login/README.md
@@ -0,0 +1,28 @@
+1. pip install werkzeug (actl idt u needa do this but sure)
+2. run login.py (python login.py) --> creates employee table in mysql
+3. populate employee table on phpmyadmin using employee_db.sql
+
+Users:
+{
+    "email": "test1@gmail.com",
+    "password": "PasswordNumber!1",
+    "role": "staff",
+    "department": "finance",
+    "employee_name": "mee" 
+}
+
+{
+    "email": "test2@gmail.com",
+    "password": "PasswordNumber!2",
+    "role": "staff",
+    "department": "finance",
+    "employee_name": "mee2" 
+}
+
+{
+    "email": "test3@gmail.com",
+    "password": "PasswordNumber!3",
+    "role": "manager",
+    "department": "finance",
+    "employee_name": "mee3" 
+}
--- backend/login/login.py
+++ backend/login/login.py
@@ -10,12 +10,14 @@ app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_recycle': 299} # do not timeout
 
 db = SQLAlchemy(app) # initialise connection to db
 
-class User(db.Model):
-    __tablename__ = 'User'
-    user_id = db.Column(db.Integer, primary_key=True)
+class Employee(db.Model):
+    __tablename__ = 'Employee'
+    employee_id = db.Column(db.Integer, primary_key=True)
+    employee_name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)
+    department = db.Column(db.String(100))
     role = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -24,36 +26,43 @@ def login():
     data = request.json
     email = data.get('email')
     password = data.get('password')
-    user = User.query.filter_by(email=email).first()
-    if not user:
-        return {"error": "User does not exist"}, 404
-    if not check_password_hash(user.password, password):
-        return {"error": "Incorrect password"}, 401
-    UID = user.user_id
-    role = user.role
 
-    return {"user_id": UID, "role": role}
+    employee = Employee.query.filter_by(email=email).first()
+
+    if not employee:
+        return {"error": "Employee does not exist"}, 404
+    if not check_password_hash(employee.password, password):
+        return {"error": "Incorrect password"}, 401
+    
+    EID = employee.employee_id
+    role = employee.role
+
+    return {"employee_id": EID, "role": role}
 
 @app.route('/register', methods=['POST'])
 def register():
-    
-    # input: {email:str, password:str, role:str}
-    # check if user already exists in database
-    # if exists, return error
-    # if does not exist, create new user and return user_id and role
+    # input: {email:str, employee_name:str, department:str, role:str, password:str}
+
     data = request.json
     email = data.get('email')
     password = data.get('password')
     role = data.get('role')
-    user = User.query.filter_by(email=email).first()
-    if user:
-        return {"error": "User already exists"}, 409
-    hashed_password = generate_password_hash(password, method='scrypt', salt_length=16)
-    new_user = User(email=email, password=hashed_password, role=role)
-    db.session.add(new_user)
+    department = data.get('department')
+    employee_name = data.get('employee_name')
+
+    employee = Employee.query.filter_by(email=email).first()
+
+    if employee:
+        return {"error": "Employee already exists"}, 409
+    
+    hashed_password = generate_password_hash(password)
+    new_employee = Employee(email=email, password=hashed_password, role=role, department=department, employee_name=employee_name)
+
+    db.session.add(new_employee)
     db.session.commit()
-    UID = new_user.user_id
-    return {"user_id": UID, "role": role}, 201
+
+    EID = new_employee.employee_id
+    return {"employee_id": EID, "role": role}, 201
 
 @app.route('/reset', methods=['POST'])
 def reset():
